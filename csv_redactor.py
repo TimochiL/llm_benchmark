7ed2cceec07d6dc0f4a19ad666e55e16518686e8
Timothy Liu <timothy.l.6uy@gmail.com>
2024-08-10 22:53:55 -0500

    Cleanup

--- csv_redactor.py
+++ csv_redactor.py
@@ -4,11 +4,11 @@ types = (2,4,'fp16')
 folder = "llama27bchat_csv"
 
 for type in types:
-    csv_path = f"{folder}\quant_{type}_questions_and_responses_0-389_redacted.csv"
+    csv_path = f"{folder}\quant_{type}_questions_and_responses_0-389.csv"
     csv_out = open(csv_path, "w", newline="")
     csv_writer = csv.writer(csv_out)
     
-    csv_path = f"{folder}\quant_{type}_questions_and_responses_0-389_reformatted.csv"
+    csv_path = f"{folder}\quant_{type}_questions_and_responses_0-389_redacted.csv"
     csv_in = open(csv_path, "r")
     
     csv_writer.writerow(csv_in.readline().strip().split(","))
@@ -16,7 +16,7 @@ for type in types:
     csv_reader = csv.reader(csv_in)
     
     for line in csv_reader:
-        csv_writer.writerow([line[0],line[1],'[REDACTED]',str(int(line[3])^1)])
+        csv_writer.writerow([line[0],line[1],line[2],'[REDACTED]',line[4]])
     
     csv_out.close()
     csv_in.close()
--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,53 @@
-import csv
+import matplotlib.pyplot as plt
+import csv, math
 
 types = (2,4,'fp16')
 folder = "llama27bchat_csv"
 
+scores = dict()
 for type in types:
+    scores[type] = 0
+
+for type in scores.keys():
     csv_path = f"{folder}\quant_{type}_questions_and_responses_0-389_redacted.csv"
-    csv_in = open(csv_path, "r")
+    csv_in = open(csv_path, "r",newline="")
     csv_in.readline()
     csv_reader = csv.reader(csv_in)
     
     num_correct = 0
     for line in csv_reader:
-        if line[3] == '1':
+        if line[4] == '1':
             num_correct += 1
 
+    scores[type] = num_correct
+    
     csv_in.close()
-    print(F"{type}: {num_correct}/390")
+
+# Build visuals
+
+quant_types = list( (str(i) + '-bit') if isinstance(i,int) else i.upper() for i in scores.keys())
+quant_scores = list(scores.values())
+
+fig, ax = plt.subplots(figsize=(6.5, 6))
+
+plt.bar(quant_types, quant_scores)
+for s in ['top', 'right']:
+    ax.spines[s].set_visible(False)
+    
+ax.xaxis.set_ticks_position('none')
+ax.yaxis.set_ticks_position('none')
+
+ax.xaxis.set_tick_params(pad = 10)
+ax.yaxis.set_tick_params(pad = 5)
+
+ax.invert_xaxis()
+
+for i in ax.patches:
+    plt.text(i.get_x()+0.8, i.get_height()+5, 
+        f"{i.get_height()} of 390\n{round(i.get_height() / 3.90, 2)}% Passed",
+        fontsize = 10, fontweight ='bold',
+        color ='grey')
+
+ax.set_title('Key-Value (KV) Cache Quantization Forbidden Prompts Evaluation',pad=0.5,y=1.07)
+
+plt.show()
